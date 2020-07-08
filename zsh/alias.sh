648ad4f3b12e3a0869c79b523f7d3e33c3b354d1
hugang <hugang0825@gmail.com>
2020-07-09 01:21:11 +0900

    alias archey3

--- zsh/alias.sh
+++ zsh/alias.sh
@@ -1,9 +1,16 @@
-alias ga='git add .'
+alias ga='git add'
+alias gaa='git add .'
 alias gc='git commit -m'
+alias gca='git commit -m "update"'
 alias gl='git log --all'
 alias gd='git diff --name-only --cached'
 alias gf='git pull'
 alias gp='git push'
+alias gr='git reset'
+alias grh='git reset --hard'
+alias grm='git reset --mixed'
+alias grs'git reset --soft'
 
 alias ra='ranger'
+alias s='archey3'
 
