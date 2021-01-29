442056bcc40b5687cc803447ce36418420b7519d
hugangOD <hugang0825@gmail.com>
2021-01-30 07:54:31 +0900

    update

--- doc/neovim/install.sh
+++ doc/neovim/install.sh
@@ -1,9 +1,13 @@
+# remove old folder
 rm ~/.config/nvim -rf
+
 # create folder
 mkdir ~/.config/nvim -p
 
 # download vim-plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+if [ ! -f "${XDG_DATA_HOME:-$HOME/.local/share}/nvim/site/autoload/plug.vim" ]; then
+	curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 
-# copy config
+# link config file
 ln -s $(pwd)/init.vim ~/.config/nvim/init.vim
