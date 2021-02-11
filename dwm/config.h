baa1cc90fac30196518c69d59966515b1dfc9d11
hugang <hugang0825@gmail.com>
2021-02-11 12:15:40 +0900

    update

--- dwm/config.h
+++ dwm/config.h
@@ -105,6 +105,7 @@ static const char *screenshotcmd[] = { "flameshot", "gui", NULL };
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_s,      spawn,          {.v = suspendcmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ 0,              XF86XK_AudioLowerVolume, spawn,          {.v = downvol } },
