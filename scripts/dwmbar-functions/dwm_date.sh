97058e652d39cb09b4732dfc1813cb43bc1f3532
hugang <hugang0825@gmail.com>
2021-03-09 12:52:55 +0900

    update

--- scripts/dwmbar-functions/dwm_date.sh
+++ scripts/dwmbar-functions/dwm_date.sh
@@ -8,9 +8,9 @@
 dwm_date () {
     printf "%s" "$SEP1"
     if [ "$IDENTIFIER" = "unicode" ]; then
-        printf "🌸 %s" "$(date "+%y-%m-%d %a %t")"
+        printf "🌸 %s" "$(date "+%y-%m-%d %a %T")"
     else
-        printf "DAT %s" "$(date "+%y-%m-%d %a %t")"
+        printf "DAT %s" "$(date "+%y-%m-%d %a %T")"
     fi
     printf "%s\n" "$SEP2"
 }
